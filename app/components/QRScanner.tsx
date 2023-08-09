ad97e554d754246dbd33e66bf85d0b6992179f67
Yesh-adithya31 <57474408+Yesh-adithya31@users.noreply.github.com>
2023-08-09 21:09:43 +0530

    QR-Version 2

--- app/components/QRScanner.tsx
+++ app/components/QRScanner.tsx
@@ -1,46 +1,57 @@
 "use client"
-import React, { useRef, useState } from 'react';
-import { BrowserMultiFormatReader } from '@zxing/library';
+import { useEffect, useRef } from 'react';
 
 const QRScanner = () => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
-  const [result, setResult] = useState('');
 
-  const startScanner = async () => {
-    const codeReader = new BrowserMultiFormatReader();
-    try {
-      const videoInputDevices = await codeReader.listVideoInputDevices();
-      if (videoInputDevices.length > 0 && videoRef.current) {
-        const constraints = {
-          video: {
-            deviceId: videoInputDevices[0].deviceId,
-          },
-        };
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        videoRef.current.srcObject = stream;
-        codeReader.decodeFromVideoDevice(videoInputDevices[0].deviceId, videoRef.current, (result, error) => {
-          if (result) {
-            setResult(result.getText());
-          }
-          if (error) {
-            console.error(error);
-          }
-        });
-      } else {
-        console.error('No video input devices found.');
+  useEffect(() => {
+    const constraints = { video: true };
+
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then((stream) => {
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+          videoRef.current.play();
+        }
+      })
+      .catch((error) => {
+        console.error('Error accessing webcam:', error);
+      });
+  }, []);
+
+  const handleVideoStream = () => {
+    if (videoRef.current) {
+      const canvas = document.createElement('canvas');
+      canvas.width = videoRef.current.videoWidth;
+      canvas.height = videoRef.current.videoHeight;
+      const context = canvas.getContext('2d');
+      
+      if (context) {
+        context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+
+        const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+        // Use a QR code library (like ZXing) to decode the imageData and extract QR code content
+        // Handle the decoded content as needed
       }
-    } catch (error) {
-      console.error(error);
     }
   };
 
   return (
     <div>
-      <video ref={videoRef} style={{ width: '100%' }} autoPlay playsInline />
-      <p>Scanned Result: {result}</p>
-      <button onClick={startScanner}>Start Scanner</button>
+      <video ref={videoRef} />
+      <button onClick={handleVideoStream}>Scan QR Code</button>
     </div>
   );
 };
 
 export default QRScanner;
+
+
+
+
+
+
+
+  
+
+
